36ac927df8b133143f8a5fb6aed8e6cee6d03fa6
markk1703 <72253930+markk1703@users.noreply.github.com>
2020-10-09 14:20:24 +0300

    Update index.php

--- index.php
+++ index.php
@@ -12,36 +12,16 @@ $studenti = array (
     array("nume"=>"Bob","prenume"=>"Alina","specializare"=>"CAL","an"=>2,"buget"=>"buget"),
     array("nume"=>"Ionescu","prenume"=>"Ciprian","specializare"=>"TCM","an"=>2,"buget"=>"taxa")
   );
-  $count = 0;
 
-  function array2Html($array, $table = true)
-  {
-      $out = '';
-      foreach ($array as $key => $value) {
-          if (is_array($value)) {
-              if (!isset($tableHeader)) {
-                  $tableHeader =
-                      '<th>' .
-                      implode('</th><th>', array_keys($value)) .
-                      '</th>';
-              }
-              array_keys($value);
-              $out .= '<tr>';
-              $out .= array2Html($value, false);
-              $out .= '</tr>';
-          } else {
-              $out .= "<td>$value</td>";
-          }
-      }
-  
-      if ($table) {
-          return '<table>' . $tableHeader . $out . '</table>';
-      } else {
-          return $out;
-      }
+  for ($row = 0; $row < 4; $row++) {
+    echo "<p><b>Row number $row</b></p>";
+    echo "<ul>";
+    for ($col = 0; $col < 3; $col++) {
+      echo "<li>".$studenti[$row][$col]."</li>";
+    }
+    echo "</ul>";
   }
 
-  array2Html($studenti);
   ?>
 
  

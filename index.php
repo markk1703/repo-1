da714287b8f1667f0c43cf440fa6a0bd7ad6d3f4
markk1703 <72253930+markk1703@users.noreply.github.com>
2020-10-09 14:18:26 +0300

    Update index.php

--- index.php
+++ index.php
@@ -7,22 +7,44 @@
 <?php
 $filename='date.txt';
 $studenti = array (
-    array("Pop","Ion","CAL",3,"buget"),
-    array("Popescu","Ana","TCM",1,"taxa"),
-    array("Bob","Alina","CAL",2,"buget"),
-    array("Ionescu","Ciprian",2,"taxa")
+    array("nume"=>"Pop","prenume"=>"Ion","specializare"=>"CAL","an"=>3,"buget"=>"buget"),
+    array("nume"=>"Popescu","prenume"=>"Ana","specializare"=>"TCM","an"=>1,"buget"=>"taxa"),
+    array("nume"=>"Bob","prenume"=>"Alina","specializare"=>"CAL","an"=>2,"buget"=>"buget"),
+    array("nume"=>"Ionescu","prenume"=>"Ciprian","specializare"=>"TCM","an"=>2,"buget"=>"taxa")
   );
+  $count = 0;
 
-  for ($row = 0; $row < 4; $row++) {
-    echo "<p><b>Row number $row</b></p>";
-    echo "<ul>";
-    for ($col = 0; $col < 3; $col++) {
-      echo "<li>".$studenti[$row][$col]."</li>";
-    }
-    echo "</ul>";
+  function array2Html($array, $table = true)
+  {
+      $out = '';
+      foreach ($array as $key => $value) {
+          if (is_array($value)) {
+              if (!isset($tableHeader)) {
+                  $tableHeader =
+                      '<th>' .
+                      implode('</th><th>', array_keys($value)) .
+                      '</th>';
+              }
+              array_keys($value);
+              $out .= '<tr>';
+              $out .= array2Html($value, false);
+              $out .= '</tr>';
+          } else {
+              $out .= "<td>$value</td>";
+          }
+      }
+  
+      if ($table) {
+          return '<table>' . $tableHeader . $out . '</table>';
+      } else {
+          return $out;
+      }
   }
+
+  array2Html($studenti);
   ?>
 
-?>
+ 
+
  </body>
 </html>

4fce209a2035529481e3dfb239e3a0b993bdfc87
markk1703 <72253930+markk1703@users.noreply.github.com>
2020-10-14 17:48:01 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,30 +1,37 @@
-<html>
- <head>
-  <title>PHP App 1</title>
- </head>
- <body>
 
 <?php
-$filename='date.txt';
-$studenti = array (
-    array("Pop","Ion","CAL",3,"buget"),
-    array("Popescu","Ana","TCM",1,"taxa"),
-    array("Bob","Alina","CAL",2,"buget"),
-    array("Ionescu","Ciprian","TCM",2,"taxa")
-  );
+$filename="date.txt";
+$fn = fopen($filename,"r");
+$dateStudenti=array();
 
-  for ($row = 0; $row < 4; $row++) {
-    echo "<p><b>Row number $row</b></p>";
-    echo "<ul>";
-    for ($col = 0; $col < 4; $col++) {
-      echo "<li>".$studenti[$row][$col]."</li>";
-    }
-    echo "</ul>";
+//citeste linie cu linie si adauga cuv citite in array
+     while(! feof($fn))  {
+  $line = fgets($fn);
+  array_push($dateStudenti,preg_split('/( |,)/',$line,-1, PREG_SPLIT_NO_EMPTY));
   }
+  fclose($fn);
 
-  ?>
+echo "Datele din fisierul 'date.txt' sunt memorate intr-un array bidimensional si sunt afisate in tabel.<br><br>";
 
- 
+//creare si afisare cap de tabel
+echo"<table id=tabel border=1> 
+<tr><th>Nume</th>
+<th>Prenume</th>
+<th>Specializare</th>
+<th>An</th>
+<th>Buget/Taxa</th>
+</tr>";
 
- </body>
-</html>
+//populare tabel cu datele din array
+for($i=0;$i<count($dateStudenti);$i+=1)
+{
+  echo "<tr>
+<td>".$dateStudenti[$i][0]."</td>
+<td>".$dateStudenti[$i][1]."</td>
+<td>".$dateStudenti[$i][2]."</td>
+<td>".$dateStudenti[$i][3]."</td>
+<td>".$dateStudenti[$i][4]."</td>
+</tr>";
+}
+
+?>

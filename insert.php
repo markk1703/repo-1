41ac3dd0f9ab411eb1c7281e62cd50a4239aee12
markk1703 <72253930+markk1703@users.noreply.github.com>
2020-10-23 00:22:23 +0300

    Update insert.php

--- insert.php
+++ insert.php
@@ -11,6 +11,7 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 </head>
+<title>Adauga inregistrare noua</title>
 <body>
   <div class="container">
 	<form method="post" action="insertProcess.php" class="form-group">

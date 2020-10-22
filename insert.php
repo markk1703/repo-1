22561eebdc16b6185faedd06d9200c582551c738
markk1703 <72253930+markk1703@users.noreply.github.com>
2020-10-23 00:20:07 +0300

    Add files via upload

--- insert.php
+++ insert.php
@@ -1,22 +1,32 @@
 <!DOCTYPE html>
 <html>
+<head>
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
 <!-- Optional theme -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
 
+<meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+  <div class="container">
 	<form method="post" action="insertProcess.php" class="form-group">
-		Nume:<br>
-		<input type="text" name="Nume" required>
-		<br>
-		Prenume:<br>
-		<input type="text" name="Prenume" required>
-		<br>
-		Varsta:<br>
-		<input type="number" min="1" name="Varsta" required>
+	<div class="form-group">
+		<label for="Nume">Nume:</label>
+		<input type="text" class="form-control" name="Nume" placeholder="Introduceti numele" required></div>
+		<br><div class="form-group">
+		<label for="Prenume">Prenume:</label>
+		<input type="text" class="form-control" name="Prenume" placeholder="Introduceti prenumele" required></div>
+		<br><div class="form-group">
+		<label for="Varsta">Varsta:</label>
+		<input type="number" class="form-control" min="1" name="Varsta" placeholder="Introduceti varsta" required></div>
 		<br><br>
 		<input type="submit" onclick="index.php" class="btn btn-success" name="save" value="Adauga">
 		<button type="button" onclick="window.location.replace('index.php')"class="btn btn-danger">Renunta</button>
 	</form>
+	</div>
   </body>
 </html>

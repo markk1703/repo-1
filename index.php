36872e23ff633e1241b2007ab27831c124d46136
markk1703 <72253930+markk1703@users.noreply.github.com>
2020-10-23 00:54:48 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <html>
-<head>
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
 <!-- Optional theme -->
@@ -8,8 +7,8 @@
 <!-- Latest compiled and minified JavaScript
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
  -->
-</head>
-<title>Database lab3</title>
+
+
  <body>
 <?php
 $host='remotemysql.com';
@@ -33,13 +32,17 @@ try {
 <button type="button" id="afis" class="btn btn-primary btn-lg">Ascunde/Afiseaza tabel</button><br><br>
 
 <button type="button" onclick="window.location.replace('insert.php')" class="btn btn-info">Adauga</button>
-
+<button type="button" id="btn_del" class="btn btn-info">Sterge</button>
+<button type="button" id="btn_mod" class="btn btn-info">Modifica</button>
+<div id="del" style.display = "none">
 <form method="post" action="deleteProcess.php" class="form-group">
   <br><label for="ID">ID:</label>
     <input type="number" min="1"  name="ID" placeholder="Stergeti in functie de ID" required>
     <input type="submit" onclick="deleteProcess.php" class="btn btn-danger" name="delete" value="Sterge">
 </form>
+</div>
 
+<div id="mod">
 <form method="post" action="modifyProcess.php" class="form-group">
 		<br><label for="ID">ID:</label>
     <input type="number" min="1"  name="ID" placeholder="Introduceti ID-ul" required>
@@ -51,6 +54,7 @@ try {
 		<input type="number" min="1" name="Varsta" placeholder="Introduceti noua varsta" required>
     <input type="submit" onclick="modifyProcess.php" class="btn btn-warning" name="modify" value="Modifica">
 </form>
+</div>
 
 
 <table class="table table-striped table-bordered table-hover" id="tabel">
@@ -88,10 +92,20 @@ echo
 
       <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
       <script>
+      document.getElementById("del").style.display = "none";
+      document.getElementById("mod").style.display = "none";
       $(function()
       {
         $('#afis').on('click',function(){
           $('#tabel').toggle();
+        });
+        $('#btn_del').on('click',function(){
+          $('#del').toggle();
+          $('#mod').hide();
+        });
+        $('#btn_mod').on('click',function(){
+          $('#mod').toggle();
+          $('#del').hide();
         });
       });
       </script>
